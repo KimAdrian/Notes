2f82d3ef22b0b7a2b90764bf9d27fc73df0e51ad
KimAdrian <adriankimutai5@gmail.com>
2022-01-19 02:41:06 +0300

    Added navigation graph

--- app/src/main/java/com/kimadrian/notes/ui/MainActivity.kt
+++ app/src/main/java/com/kimadrian/notes/ui/MainActivity.kt
@@ -2,11 +2,22 @@ package com.kimadrian.notes.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 import com.kimadrian.notes.R
+import com.kimadrian.notes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addNewNoteFab.setOnClickListener {
+            findNavController(R.id.myNavHostFragment).navigate(R.id.action_notesHomeFragment_to_newNoteFragment)
+        }
+
     }
+
 }
